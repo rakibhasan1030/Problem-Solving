1534b51ba829782cd09672945e87dc75a53c508b
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-24 13:32:29 +0600

    Create 1221. Split a String in Balanced Strings (String).cpp

---
+++ LeetCode/Easy/1221. Split a String in Balanced Strings (String).cpp
@@ -0,0 +1,17 @@
+    int s_size = s.size();
+    int r_count = 0, l_count = 0, main_count = 0;
+    for(int i = 0; i < s_size; i++)
+    {
+        if(s[i] == 'R'){
+            r_count++;
+        }else if(s[i] == 'L'){
+            l_count++;
+        }
+        if(r_count == l_count){
+            main_count++;
+            r_count = 0;
+            l_count = 0;
+        }
+    }
+    cout<< main_count << endl;
+    return main_count;
