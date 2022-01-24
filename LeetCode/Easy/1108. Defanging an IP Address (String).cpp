2901bcada8dbb52b2bc2166f6ddd3b239a908e91
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-24 13:27:30 +0600

    Create 1108. Defanging an IP Address (String).cpp

---
+++ LeetCode/Easy/1108. Defanging an IP Address (String).cpp
@@ -0,0 +1,13 @@
+    string str ="";
+    for(int i = 0; i < address.size(); i++)
+    {
+        if(address[i]=='.'){
+            str.push_back('[');
+            str.push_back('.');
+            str.push_back(']');
+        }
+        else{
+            str.push_back(address[i]);
+        }
+    }
+    return str;
