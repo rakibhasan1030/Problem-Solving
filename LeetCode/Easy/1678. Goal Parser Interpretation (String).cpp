9a47f2be12d8ec9a7fbd3c998f5c9ac062133625
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-24 13:31:16 +0600

    Create 1678. Goal Parser Interpretation (String).cpp

---
+++ LeetCode/Easy/1678. Goal Parser Interpretation (String).cpp
@@ -0,0 +1,21 @@
+    string str = "";
+    int commandSize = command.size();
+    for(int i = 0; i < commandSize; i++)
+    {
+        if(command[i] == 'G')
+        {
+            str = str + "G";
+        }
+        else if (command[i] == '(')
+        {
+            if(command[i+1] == ')')
+            {
+                str = str + "o";
+            }
+            else
+            {
+                str = str + "al";
+            }
+        }
+    }
+    return str;
