21bc6593532a640b9ccd082fc75ca2dfaae6146b
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-25 19:12:15 +0600

    Update 824. Goat Latin (String).cpp

--- LeetCode/Easy/824. Goat Latin (String).cpp
+++ LeetCode/Easy/824. Goat Latin (String).cpp
@@ -3,32 +3,55 @@
 using namespace std;
 int main()
 {
-    string sentence = "I speak Goat Iatin", t = "", result;
+    string sentence = "I speak Goat Latin", t = "", result;
     int space = 0;
-    for(int i = 0; i <= sentence.size(); i++){
-        if (!isspace(sentence[i]) || sentence[i] != '\0') t += sentence[i];
-
+    for(int i = 0; i <= sentence.size(); i++)
+    {
         if (isspace(sentence[i]) || sentence[i] == '\0'){
             space++;
-            
+
             if(t[0] == 'a' || t[0] == 'A' ||
                     t[0] == 'e' || t[0] == 'E' ||
                     t[0] == 'i' || t[0] == 'I' ||
                     t[0] == 'o' || t[0] == 'O' ||
                     t[0] == 'u' || t[0] == 'U'){
-
-                //cout<< "before : " << t <<endl;
+                        int tempSpace = space;
                 t += "ma";
-                cout<< space << endl;
-                while(space){
+                while(tempSpace){
                     t += "a";
-                    //cout<< space << " : " << t << endl;
-                    space--;
+                    tempSpace--;
                 }
-                //cout<< "after : " << t <<endl;
+            }else{
+
+                //cout<< space << endl;
+                //cout << endl;
+                string temp;
+                char firstValue;
+
+                for(int j = 0; j < t.size(); j++){
+                    if (j == 0){
+                        firstValue = t[j];
+                    }else{
+                        temp += t[j];
+                    }
+                }
+                temp.push_back(firstValue);
+                temp += "ma";
+                int tempSpace = space;
+                while(tempSpace){
+                    temp += "a";
+                    tempSpace--;
+                }
+                //cout << temp << endl;
+                t += temp;
+                temp = "";
             }
+
             result += t + " ";
+            cout << result << endl;
             t = "";
+        } else {
+            t += sentence[i];
         }
     }
     //cout<< result << endl;
