f5f29cf7d9baa837188f0ce65707d662ec9a3a5f
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-25 12:36:29 +0600

    Create 2042. Check if Numbers Are Ascending in a Sentence (String).cpp

---
+++ LeetCode/Easy/2042. Check if Numbers Are Ascending in a Sentence (String).cpp
@@ -0,0 +1,23 @@
+    string x = "";
+    vector<int> numbers;
+    bool w;
+    for (int i = 0; i <= s.size(); i++){
+        x.push_back(s[i]);
+        if(isspace(s[i]) || s[i] == '\0')
+        {
+            bool n = x.find_first_not_of("0123456789");
+            if(n == 1){
+                numbers.push_back(stoi(x));
+            }
+            x = "";
+        }
+    }
+    for (int j = 1; j < numbers.size(); j++){
+        if (numbers[j-1] < numbers[j]){
+            w = true;
+        }else{
+            w = false;
+            break;
+        }
+    }
+    return w;
