7d16c586fb55e7b105e556eb047703b56d24f487
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-25 23:50:05 +0600

    Update 824. Goat Latin (String).cpp

--- LeetCode/Easy/824. Goat Latin (String).cpp
+++ LeetCode/Easy/824. Goat Latin (String).cpp
@@ -1,59 +1,67 @@
-
-#include<bits/stdc++.h>
-using namespace std;
-int main()
+string t = "", result;
+int space = 0;
+for(int i = 0; i <= sentence.size(); i++)
 {
-    string sentence = "I speak Goat Latin", t = "", result;
-    int space = 0;
-    for(int i = 0; i <= sentence.size(); i++)
+    if (isspace(sentence[i]) || sentence[i] == '\0')
     {
-        if (isspace(sentence[i]) || sentence[i] == '\0'){
-            space++;
+        space++;
 
-            if(t[0] == 'a' || t[0] == 'A' ||
-                    t[0] == 'e' || t[0] == 'E' ||
-                    t[0] == 'i' || t[0] == 'I' ||
-                    t[0] == 'o' || t[0] == 'O' ||
-                    t[0] == 'u' || t[0] == 'U'){
-                        int tempSpace = space;
-                t += "ma";
-                while(tempSpace){
-                    t += "a";
-                    tempSpace--;
-                }
-            }else{
-
-                //cout<< space << endl;
-                //cout << endl;
-                string temp;
-                char firstValue;
-
-                for(int j = 0; j < t.size(); j++){
-                    if (j == 0){
-                        firstValue = t[j];
-                    }else{
-                        temp += t[j];
-                    }
-                }
-                temp.push_back(firstValue);
-                temp += "ma";
-                int tempSpace = space;
-                while(tempSpace){
-                    temp += "a";
-                    tempSpace--;
-                }
-                //cout << temp << endl;
-                t += temp;
-                temp = "";
+        if(t[0] == 'a' || t[0] == 'A' ||
+                t[0] == 'e' || t[0] == 'E' ||
+                t[0] == 'i' || t[0] == 'I' ||
+                t[0] == 'o' || t[0] == 'O' ||
+                t[0] == 'u' || t[0] == 'U')
+        {
+            int tempSpace = space;
+            t += "ma";
+            while(tempSpace)
+            {
+                t += "a";
+                tempSpace--;
             }
+            if (space != 1)
+            {
+                result += " " +  t;
+            }
+            else result += t;
 
-            result += t + " ";
-            cout << result << endl;
-            t = "";
-        } else {
-            t += sentence[i];
         }
+        else
+        {
+            string temp;
+            char firstValue;
+
+            for(int j = 0; j < t.size(); j++)
+            {
+                if (j == 0)
+                {
+                    firstValue = t[j];
+                }
+                else
+                {
+                    temp += t[j];
+                }
+            }
+            temp.push_back(firstValue);
+            temp += "ma";
+            int tempSpace = space;
+            while(tempSpace)
+            {
+                temp += "a";
+                tempSpace--;
+            }
+            if (space != 1)
+            {
+                result += " " + temp;
+            }
+            else result += temp;
+            temp = "";
+        }
+        t = "";
     }
-    //cout<< result << endl;
-    return 0;
+    else
+    {
+        t += sentence[i];
+    }
 }
+return result;
