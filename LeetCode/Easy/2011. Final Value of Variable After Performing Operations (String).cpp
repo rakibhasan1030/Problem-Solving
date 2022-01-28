0a96b582c492739d4b1bb12e82406ab99bd8483a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-28 23:58:53 +0600

    Update 2011. Final Value of Variable After Performing Operations (String).cpp

--- LeetCode/Easy/2011. Final Value of Variable After Performing Operations (String).cpp
+++ LeetCode/Easy/2011. Final Value of Variable After Performing Operations (String).cpp
@@ -1,10 +1,10 @@
         int x = 0;
         for(int i = 0; i < operations.size(); i++){
-        
+
             if(operations[i] == "X++" || operations[i] == "++X"){
                 x++;
             }else if(operations[i] == "X--" || operations[i] == "--X"){
                 x--;
             }
-        }       
+        }
         return x;
