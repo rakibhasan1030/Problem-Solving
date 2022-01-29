b942ebbc12030be4d25a1e842f16014f26d0496a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-29 13:16:39 +0600

    Update 1078. Occurrences After Bigram (String).cpp

--- LeetCode/Easy/1078. Occurrences After Bigram (String).cpp
+++ LeetCode/Easy/1078. Occurrences After Bigram (String).cpp
@@ -1,50 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    std::vector<string> vec, result, x, y;
-    string text = "alice is aa good girl she is a good student", first = "a", second = "good", temp, t;
-    for (int i = 0; i < text.size()+1; i++)
-    {
-        if(text[i] != ' ') temp = temp + text[i];
-        if(text[i] == ' ' || text[i] == '\0'){
-
-            vec.push_back(temp);
-            temp = "";
+        vector<string>store,ans;
+        stringstream ss(text);
+        string word;
+        while(ss >> word)
+        {
+            store.push_back(word);
         }
-    }
-
-    for(int j = 0 ; j < vec.size(); j++){
-
-        if (vec[j] == first){
-            x.push_back(vec[j+2]);
-            if(j == vec.size() - 1){
-                t = x[x.size()-1];
-                //cout<< x[x.size()-1] << endl;
-                result.push_back(t);
-                t = "";
-            }
-        } else if (vec[j] == second){
-            y.push_back(vec[j+1]);
-            if(j == vec.size() - 1){
-                t = y[y.size()-1];
-                //cout<< t<< endl;
-                result.push_back(t);
-                t = "";
+        
+        int n = store.size();
+        for(int i=0;i<n;i++)
+        {
+            if(store[i]==first && store[i+1]==second && i+2<n)
+            {
+                ans.push_back(store[i+2]);
             }
         }
-
-
-
-    }
-
-    //cout << x[x.size()-1] <<endl;
-
-    //std::sort(result.begin(), result.end());
-    //result.erase(std::unique(result.begin(), result.end()), result.end());
-
-    for(auto s : result){
-        //cout << s << " ";
-    }
-    return 0;
-}
+        return ans;
