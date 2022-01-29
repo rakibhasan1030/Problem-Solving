33c01a7961336e5c7dc61c548410f8bc9079aedc
Rakib Hasan <rakibhasan1030@gmail.com>
2022-01-29 18:00:27 +0600

    Create 1446. Consecutive Characters (String).cpp

---
+++ LeetCode/Easy/1446. Consecutive Characters (String).cpp
@@ -0,0 +1,11 @@
+int _temp = 1, _max = 1;
+if(s.size() == 1) return 1;
+for(int i = 1; i <= s.size(); i++){
+	if (s[i-1] == s[i]){
+		_temp++;
+	}else{
+		_temp = 1;
+	}
+	if(_temp > _max) _max = _temp;
+}
+return _max;
