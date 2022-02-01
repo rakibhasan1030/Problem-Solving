4d383230944ed64615d0e25861faf196373d60af
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-01 18:46:37 +0600

    Update 1417. Reformat The String (String).cpp

--- LeetCode/Easy/1417. Reformat The String (String).cpp
+++ LeetCode/Easy/1417. Reformat The String (String).cpp
@@ -1,9 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s = "a0b1c2";
-
-
-
-    retutn 0;
+    string s = "a0b1c2", ch, digits;
+    int maxSize = 0, chSize, digitsSize;
+    for(auto i : s){
+        if (isdigit(i)){
+            ch.push_back(i);
+        }else{
+            digits.push_back(i);
+        }
+    }
+    chSize = ch.size();
+    digitsSize = digits.size();
+    maxSize = chSize > digitsSize ? maxSize = chSize : maxSize = digitsSize;
+    cout << ch << " : " << digits << "   |   " << maxSize;
+    return 0;
 }
