e45a8b9e4dd665d69c76b57d5315b8dc9426c2f3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-01 23:13:29 +0600

    Update 1417. Reformat The String (String).cpp

--- LeetCode/Easy/1417. Reformat The String (String).cpp
+++ LeetCode/Easy/1417. Reformat The String (String).cpp
@@ -1,32 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s = "covid2019", ch, digits, ans;
-    int diffSize, chSize, digitsSize;
+    string s = "j", ch, digits, ans;
+    int diffSize = 0, chSize = 0, digitsSize = 0;
     for(auto i : s){
         if (isdigit(i) && i != '\0'){
-            digits.push_back(i);
+            digits += i;
         }else if (!isdigit(i) && i != '\0'){
-            ch.push_back(i);
+            ch += i;
         }
     }
     chSize = ch.size();
     digitsSize = digits.size();
     diffSize = ch.size() - digits.size();
-    cout<< chSize << "   :   " << digitsSize << endl;
+    cout<< chSize << " - " << digitsSize << " = " <<diffSize << endl;
+
     if(diffSize == -1 || diffSize == 0 ||diffSize == 1){
 
         for (int j = 0; j < s.size(); j++){
-            if(j <= chSize && ch[j] != '\0'){
-                ans.push_back(ch[j]);
-            }
-            if(j <= digitsSize && ch[j] != '\0'){
-                ans.push_back(digits[j]);
+
+            if (chSize > digitsSize){
+                if(j <= chSize){
+
+                    ans += ch[j];
+                }
+                if(j <= digitsSize){
+                    ans += digits[j];
+                }
+            } else{
+                if(j <= digitsSize){
+                    ans += digits[j];
+                }
+                if(j <= chSize){
+                    ans += ch[j];
+                }
             }
         }
     }else {
         ans = "";
     }
-    cout<< ans;
+
+    if (!ans.empty()){
+        if(chSize > 0){
+            ans.pop_back();
+        }
+        if(digitsSize > 0){
+            ans.pop_back();
+        }
+    }
+    cout<< ans.size()<< " : " << ans;
     return 0;
 }
