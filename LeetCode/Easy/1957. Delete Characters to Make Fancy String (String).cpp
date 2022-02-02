a10a13ba1f29da975417a53454abd0492841dd49
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-02 15:42:56 +0600

    Update 1957. Delete Characters to Make Fancy String (String).cpp

--- LeetCode/Easy/1957. Delete Characters to Make Fancy String (String).cpp
+++ LeetCode/Easy/1957. Delete Characters to Make Fancy String (String).cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    string s = "leeetcode";
+    int i = 0, c = 1;
 
+    for(i, i < s.size(); i++){
 
+    }
+
+    s.erase(0,1);
+
+    cout<< s << endl;
     return 0;
 }
