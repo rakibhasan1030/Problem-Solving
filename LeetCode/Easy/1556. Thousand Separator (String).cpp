31d90155bd4363eb322c8665a4f5693b1a3d3e6e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-02 14:44:56 +0600

    Update 1556. Thousand Separator (String).cpp

--- LeetCode/Easy/1556. Thousand Separator (String).cpp
+++ LeetCode/Easy/1556. Thousand Separator (String).cpp
@@ -1,18 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n = 1987, nSize;
-    string ans, s = "";
+class Solution {
+public:
+    string thousandSeparator(int n) {
+    int nSize, c = 0;
+    string s = "", dot = ".";
     s = to_string(n);
     nSize = s.size();
 
     if (nSize <= 3){
-        ans = s;
+
     }else{
         for(int i = s.size()-1; i >= 0; i--){
-            cout << s[i] << endl;
+            c++;
+            if(c % 3 == 0 && i != 0){
+                s.insert(i, dot);
+            }
         }
     }
-    cout << ans << endl;
-    return 0;
-}
+    return s;
+    }
+};
