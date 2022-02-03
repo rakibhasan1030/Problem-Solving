fd7bd74a74c2eb6ca6dbb7f905d429d72952821f
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-03 15:34:12 +0600

    Update 1957. Delete Characters to Make Fancy String (String).cpp

--- LeetCode/Easy/1957. Delete Characters to Make Fancy String (String).cpp
+++ LeetCode/Easy/1957. Delete Characters to Make Fancy String (String).cpp
@@ -5,8 +5,8 @@ public:
     int i = 0, j = 0;
     for(i; i < s.size(); i++){
         if(i < 2){
-            ans += s[i]; //aa
-            j++; //1 [0,1,2]
+            ans += s[i];
+            j++;
         }else{
             if((ans[j-1] != s[i]) || (ans[j-2] != s[i])){
                 ans += s[i];
