9ca7d16c470fa5d22d8ba3ea7b96fe2a850abbe4
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-03 18:28:48 +0600

    Update 520. Detect Capital (String).cpp

--- LeetCode/Easy/520. Detect Capital (String).cpp
+++ LeetCode/Easy/520. Detect Capital (String).cpp
@@ -2,9 +2,24 @@
 using namespace std;
 int main(){
 
-    string word = "USA";
-
-
-
+    string word = "FlaG";
+    int n, upper = 0, lower = 0;
+    n = word.size();
+    for (int i = 0; i< n; i++){
+            if(isupper(word[i])){
+                upper++;
+            }else{
+                lower++;
+            }
+    }
+    if (upper == n || lower == n){
+        cout<< "true";
+    }else {
+        if (upper == 1 && isupper(word[0])){
+            cout << "true";
+        }else{
+            cout << "false";
+        }
+    }
     return 0;
 }
