397cc7f85b110aeee790a4ba94658b0ecc6f05ad
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-05 14:17:53 +0600

    Update 1576. Replace All 's to Avoid Consecutive Repeating Characters (String).cpp

--- LeetCode/Easy/1576. Replace All 's to Avoid Consecutive Repeating Characters (String).cpp
+++ LeetCode/Easy/1576. Replace All 's to Avoid Consecutive Repeating Characters (String).cpp
@@ -6,13 +6,7 @@ int main(){
     for (int i = 0; i < n; i++){
 
         if(s[i] == '?'){
-            if (i == 0 && s[i+1] == 'z' || i == n - 1 && s[i-1] == 'z'){
-                ans += char((int)s[i+1] - 1);
-            }else if(i == 0 && s[i+1] == 'a' || i == n - 1 && s[i-1] == 'a'){
-                ans += char((int)s[i+1] + 1);
-            }else{
-                ans += char((int)s[i+1] + 1);
-            }
+
         }else{
             ans += s[i];
         }
