57956b96975a5cb746c004c1aa027c9ac5c36b0a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-07 01:34:56 +0600

    Update 1592. Rearrange Spaces Between Words (String).cpp

--- LeetCode/Easy/1592. Rearrange Spaces Between Words (String).cpp
+++ LeetCode/Easy/1592. Rearrange Spaces Between Words (String).cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main(){
     string text = "  this   is  a sentence ";
-    int string_len, total_space, total_word, between_space, end_space;
+    int i, string_len, total_space, total_word, between_space, end_space;
     string_len = text.size();
-    for (int i = 0; i < string_len; i++){
-        if(text [i] == ' '){
-            total_space++;
-        }
+    char prev_char;
 
-    }
+
+    i = 0;
+    total_word = 0;
+    total_space = 0;
+    prev_char = '\0';
+
+
     cout << total_space;
 }
