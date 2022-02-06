2440c30cc50262a65dc46311f18309b87fab4265
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-07 00:39:04 +0600

    Update 551. Student Attendance Record I (String).cpp

--- LeetCode/Easy/551. Student Attendance Record I (String).cpp
+++ LeetCode/Easy/551. Student Attendance Record I (String).cpp
@@ -4,13 +4,14 @@ int main(){
 
     string s = "PPALLP";
     int n, a = 0, l = 0;
+    bool ans = true;
     n = s.size();
 
     for(int i = 0; i < n; i++){
         if(s[i] == 'A') a++;
-        else if (s[i] == 'P' && s[i+1] == 'P' && s[i+2] == 'P') break;
+        else if (s[i] == 'P' && s[i+1] == 'P' && s[i+2] == 'P') ans = false; break;
     }
+    if(a >= 2) ans = false;
 
-
-    retutn 0;
+    cout << ans;
 }
