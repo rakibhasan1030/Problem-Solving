f95fb41f49b44da0a28381ecfc935052fde1efc5
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-12 00:41:53 +0600

    Update 482. License Key Formatting (String).cpp

--- LeetCode/Easy/482. License Key Formatting (String).cpp
+++ LeetCode/Easy/482. License Key Formatting (String).cpp
@@ -5,7 +5,12 @@ int main(){
     string s = "5F3Z-2e-9-w", temp, ans;
     int k = 4;
 
+    for(int i = 0; i <= s.size(); i++){
+        if(s[i] != '-' || s[i] = '\0'){
+            temp += temp + s[i];
+        }
+    }
 
-
+    cout<< temp;
     return ans;
 }
