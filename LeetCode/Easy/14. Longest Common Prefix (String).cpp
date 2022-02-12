1d72e89cadc9f1a7a59acdb28156fd3589fa782d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-13 00:22:34 +0600

    Update 14. Longest Common Prefix (String).cpp

---
+++ LeetCode/Easy/14. Longest Common Prefix (String).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string> strs = ["flower","flow","flight"];
+    if(strs.size() == 0) return "";
+
+    string prefix = strs[0];
+    for (int i = 1; i < strs.size(); i++){
+        while(strs[i])
+    }
+
+    return 0;
+}
