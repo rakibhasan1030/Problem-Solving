2eabf9bc6b54124fbff314eebbec41865fedd615
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-12 16:17:05 +0600

    Update 1784. Check if Binary String Has at Most One Segment of Ones (String).cpp

--- LeetCode/Easy/1784. Check if Binary String Has at Most One Segment of Ones (String).cpp
+++ LeetCode/Easy/1784. Check if Binary String Has at Most One Segment of Ones (String).cpp
@@ -1,14 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s = "10";
+    string s = "1100";
     int sz = s.size(), c = 0;
     bool ans;
 
     for(int i = 0; i < sz; i++){
-        if(s[i] == '1' && s[i+1] == '1' || s[i+1] != '\0'){
+        if(s[i] == '1' && s[i+1] == '1'){
             c++;
         }
+    }
+    if (sz > 1){
+        if(c == 1){
+            ans = true;
+        }else{
+            ans = false;
+        }
+    }else{
+        ans = false;
     }
     cout << ans;
     return ans;
