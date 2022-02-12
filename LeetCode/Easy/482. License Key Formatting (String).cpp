6ceb4bc496c570ee0977b750f26d55e7691dd3df
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-12 13:00:37 +0600

    Update 482. License Key Formatting (String).cpp

--- LeetCode/Easy/482. License Key Formatting (String).cpp
+++ LeetCode/Easy/482. License Key Formatting (String).cpp
@@ -4,6 +4,7 @@ int main(){
 
     string s = "a-a-a-a-", temp, ans;
     int k = 1, sz, firstGroupQuantity, restOfGroupQuantity;
+    vector<string> arr;
 
     sz = s.size();
 
@@ -13,27 +14,28 @@ int main(){
         }
     }
 
+
     //calculation
     int tempS = temp.size();
     firstGroupQuantity = tempS % k;
 
-
+    //cout << "temp : " << temp << "   "  << tempS <<endl;
 
     for(int j = 0; j <= tempS; j++){
         ans += temp[j];
         if(firstGroupQuantity > 0 && j+1 == firstGroupQuantity && temp[j+1] != '\0'){
             ans += '-';
-            cout << "j : " << j <<endl;
+            cout << "temp[" << j << "]" << "   -   " << temp[j] << endl;
         }
         cout << "ch = " << temp[j] << "  " << "ch + 1 = " << temp[j+1] << endl;
 
         if(j+1 == firstGroupQuantity+k && temp[j+1] != '\0'){
              ans += '-';
-             cout << "j : " << j+1 <<endl;
              k += k;
+             //cout << "temp[" << j << "]" << "   -   " << temp[j] << endl;
         }
     }
     transform(ans.begin(), ans.end(), ans.begin(), ::toupper);
-    cout << "firstGroupQuantity = " << firstGroupQuantity << "   |   " << "tempS = " << tempS << "   |   " << ans;
+    //cout << "firstGroupQuantity = " << firstGroupQuantity << "   |   " << "tempS = " << tempS << "   |   " << ans;
     return 0;
 }
