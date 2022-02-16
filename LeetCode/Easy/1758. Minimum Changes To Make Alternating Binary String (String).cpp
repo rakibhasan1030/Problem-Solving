dc01e302a6cdf08594302a71a3ec94e4e476f24e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-16 14:36:32 +0600

    Update 1758. Minimum Changes To Make Alternating Binary String (String).cpp

--- LeetCode/Easy/1758. Minimum Changes To Make Alternating Binary String (String).cpp
+++ LeetCode/Easy/1758. Minimum Changes To Make Alternating Binary String (String).cpp
@@ -4,12 +4,8 @@ int main(){
     string s = "01001110";
 
     for(int i = 0; i < s.size(); i++){
+
         if (s[i+1] != '\0'){
-            if (s[i] == '0'){
-                s = s[i+1] == 1 ? 1 : 1;
-            }else{
-                s = s[i+1] == 0 ? 0 : 0;
-            }
         }
     }
 
