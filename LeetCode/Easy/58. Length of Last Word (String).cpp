5c5a765c0ce8a455c40b84918e5bb3e5049ff4a9
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-17 00:29:51 +0600

    Update 58. Length of Last Word (String).cpp

--- LeetCode/Easy/58. Length of Last Word (String).cpp
+++ LeetCode/Easy/58. Length of Last Word (String).cpp
@@ -4,8 +4,9 @@ int main(){
     string s = "   fly me   to   the moon  ";
     vector<string> st;
     int c;
-    for(int i = 0; i <= s.size(); i++){
-        if(s[i] != ' ' && s[i+1] == ' ' || s[i+1] != '\0'){
+    for(int i = 0; i < s.size(); i++){
+        if(ischar && s[i+1] == ' ' || s[i+1] == '\0'){
+            cout<< s[i];
             c++;
         }
     }
