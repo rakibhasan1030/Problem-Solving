64c54ed0c84df84ff3a71a91cb67a0533a60c4e6
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-16 17:47:54 +0600

    Update 434. Number of Segments in a String (String).cpp

--- LeetCode/Easy/434. Number of Segments in a String (String).cpp
+++ LeetCode/Easy/434. Number of Segments in a String (String).cpp
@@ -1,15 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    string s = "Hello, my name is John";
+class Solution {
+public:
+    int countSegments(string s) {
     int word = 0;
-    if (s.size() > 0){
-        for(int i = 0; i < s.size(); i ++){
-            if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0'){
-                word++;
-            }
+    for(int i = 0; i < s.size(); i ++){
+        if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0'){
+            word++;
         }
     }
-    cout << word;
-    return 0;
-}
+    return word;
+    }
+};
