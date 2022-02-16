d5f7505976629b8e26cd13f164dd54cb6f7c01ed
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-16 18:07:11 +0600

    Update 434. Number of Segments in a String (String).cpp

--- LeetCode/Easy/434. Number of Segments in a String (String).cpp
+++ LeetCode/Easy/434. Number of Segments in a String (String).cpp
@@ -1,6 +1,8 @@
+/*APPROACH:
 
-
-
+Given string s contains numbers of segment (words) which is separated by space and segment can contain alphabets, numbers and special characters.
+I've check if the position of i contains no space (that means s[ i ] will be alphabets, numbers or special characters), then i also check the next position (i+1) is a space or null (end of string) so that i can find segment and increase word
+Finally, return word */
 
 class Solution {
 public:
