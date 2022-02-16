dfe291819e7ae0a9d82b9ea0d719177c2a288526
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-16 18:12:07 +0600

    Update 1758. Minimum Changes To Make Alternating Binary String (String).cpp

--- LeetCode/Easy/1758. Minimum Changes To Make Alternating Binary String (String).cpp
+++ LeetCode/Easy/1758. Minimum Changes To Make Alternating Binary String (String).cpp
@@ -1,3 +1,13 @@
+/* APPROACH:
+# Given String s has two possible way to occur with first position, either its 0 or 1.
+# If it stats with 0, then all the even position contain 0 and also the odd position contain 1.
+  For i, if s[ i ] do not follow the rule then we increase y
+# On the other hand if it starts with 1 then all the even position contain 1 and also the odd position contain 0. and similarly,
+  For i, if s[ i ] do not follow the rule then we increase x
+# After that we find and return the minimum between x and y */
+
+
+
 class Solution {
 public:
     int minOperations(string s){
