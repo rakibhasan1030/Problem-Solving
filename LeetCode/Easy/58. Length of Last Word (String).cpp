c416acd3fcb2990a9d15d466082e8c1c9338989d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-17 00:58:47 +0600

    Update 58. Length of Last Word (String).cpp

--- LeetCode/Easy/58. Length of Last Word (String).cpp
+++ LeetCode/Easy/58. Length of Last Word (String).cpp
@@ -1,21 +1,20 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    string s = "luffy is still joyboy", temp;
-    vector<string> st;
-    for(int i = 0; i < s.size(); i++){
-        if(isalpha(s[i])){
-            temp += s[i];
-            if(isalpha(s[i]) && s[i+1] == ' ' || isalpha(s[i]) && s[i+1] == '\0'){
-                st.push_back(temp);
-                cout << temp << "   ";
-                temp = "";
-            }
+/* APPROACH:
+# Given string s contains numbers of segment (words) which is separated by space and segment can contain alphabets,
+  numbers and special characters.
+# I've check if the position of i contains no space (that means s[ i ] will be alphabets, numbers or special characters),
+  then i also check the next position (i+1) is a space or null (end of string) so that i can find segment and increase word
+# Finally, return word */
+
+
+class Solution {
+public:
+    int countSegments(string s) {
+    int word = 0;
+    for(int i = 0; i < s.size(); i ++){
+        if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0'){
+            word++;
         }
     }
-
-    cout << st[st.size()-1].size() << endl;
-
-    return 0;
-}
-
+    return word;
+    }
+};
