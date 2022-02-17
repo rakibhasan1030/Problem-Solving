0867867522a5e34fe886f01523f394822da28b75
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-18 01:41:49 +0600

    Update 830. Positions of Large Groups (String).cpp

--- LeetCode/Easy/830. Positions of Large Groups (String).cpp
+++ LeetCode/Easy/830. Positions of Large Groups (String).cpp
@@ -2,8 +2,15 @@
 using namespace std;
 int main(){
     string s = "abbxxxxzzy";
+    int c;
 
+    for(int i = 0; i < s.size(); i++){
 
+        if(s[i] == s[i+1]){
+            c++;
+        }
+    }
+    cout<< c ;
 
     return 0;
 }
