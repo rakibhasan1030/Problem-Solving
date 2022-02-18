9a58f9bbcc554268de5e3fa5a288a23a924671b3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-18 18:24:22 +0600

    Update 2047. Number of Valid Words in a Sentence (String).cpp

--- LeetCode/Easy/2047. Number of Valid Words in a Sentence (String).cpp
+++ LeetCode/Easy/2047. Number of Valid Words in a Sentence (String).cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string sentence = "cat and  dog";
+    string sentence = "cat and  dog", temp;
+
+    for(int i = 0; i < sentence.size(); i++){
+        if (sentence[i] != ' '){
+            temp += sentence[i];
+        }
+        if(sentence[i+1] == ' ' || sentence[i+1] == '\0'){
+            cout << temp << endl;
+        }
+
+    }
+
 
     return 0;
 }
