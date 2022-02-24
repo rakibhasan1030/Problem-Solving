f1663f7acc50d763e37616b2f9a5bbdaaf1ba56c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-24 16:57:36 +0600

    "LeetCode/Easy/1. Two Sum.cpp": update

---
+++ LeetCode/Easy/1. Two Sum.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int nums[] = {3,2,4};
+    int target = 6;
+
+    for(int i = 0; i < nums.length(); i++){
+
+    }
+
+    return 0;
+}
