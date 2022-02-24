18e07abf8d582e8b35ac40dc4491f6fa4d2aa624
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-24 14:37:27 +0600

    update

--- LeetCode/Medium/Test-File.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-    void helllo() {
-        cout << "Hello World!" << endl;
-    }
-};
- 
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#include "Medium/Test-File.cpp"
-using namespace std;
-int main(){
-    
-    Solution s;
-    s.helllo();
-    return 0;
-}
