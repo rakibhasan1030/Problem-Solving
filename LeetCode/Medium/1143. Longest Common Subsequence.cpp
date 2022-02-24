15052a2731e6c6c09c666628fc31024ee9b1651e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-25 02:56:54 +0600

    "LeetCode/Medium/1143. Longest Common Subsequence.cpp":

--- LeetCode/Medium/1143. Longest Common Subsequence.cpp
+++ LeetCode/Medium/1143. Longest Common Subsequence.cpp
@@ -7,9 +7,11 @@ public:
     {
         int len1 = text1.length();
         int len2 = text2.length();
+
         int res [len1 + 1][len2 + 1];
-        for(int i = 0; i < len1; i++){
-            for(int j = 0; j < len2; j++){
+
+        for(int i = 0; i <= len1; i++){
+            for(int j = 0; j <= len2; j++){
                 if(i == 0 && j == 0){
                     res[i][j] = 0;
                 }else if (text1[i-1] == text2[j-1]){
