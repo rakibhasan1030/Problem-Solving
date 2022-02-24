156883712b7cd10f390cd620fba55cb8697a745e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-24 17:29:02 +0600

    "LeetCode/Easy/1. Two Sum.cpp": update

--- LeetCode/Easy/1. Two Sum.cpp
+++ LeetCode/Easy/1. Two Sum.cpp
@@ -1,14 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    void twoSum(vector<int>& nums, int target) {
-    int nums[] = {3,2,4};
-    int target = 6;
-    int n = sizeof(nums)/sizeof(nums[0]);
+    void twoSum()
+    {
+        int nums[] = {3, 2, 4};
+        int target = 6;
+        int n = sizeof(nums) / sizeof(nums[0]);
 
-    for(int i = 0; i < n; i++){
-        cout << nums[i] << " ";
-    }
+        for (int i = 0; i < n; i++)
+        {
+            cout << nums[i] << " ";
+        }
     }
 };
