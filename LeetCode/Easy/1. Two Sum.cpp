c8d10e16a66937a7eac28f4d8bf5602994475820
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-24 17:51:28 +0600

    "LeetCode/Easy/1. Two Sum.cpp": update

--- LeetCode/Easy/1. Two Sum.cpp
+++ LeetCode/Easy/1. Two Sum.cpp
@@ -8,10 +8,20 @@ public:
         int nums[] = {3, 2, 4};
         int target = 6;
         int n = sizeof(nums) / sizeof(nums[0]);
+        vector<int> ans;
 
         for (int i = 0; i < n; i++)
         {
-            cout << nums[i] << " ";
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j && (nums[i] + nums[j] == target))
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    cout << "i = " << i << "\nj ==" << j << endl;
+                    break;
+                }
+            }
         }
     }
 };
