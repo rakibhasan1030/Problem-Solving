eb9732231594d087275a0342afde2713119bbf2c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-25 21:15:02 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
-#include "1. Two Sum.cpp"
+#include "228. Summary Ranges.cpp"
 using namespace std;
 int main()
 {
-    vector<int> nums;
-    nums.push_back(3);
-    nums.push_back(2);
-    nums.push_back(4);
     Solution s;
-    s.twoSum(nums, 6);
+    
     return 0;
 }
