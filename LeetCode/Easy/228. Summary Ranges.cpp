3db008d6e38984e96034d5ef4c2ebbd29e820d80
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-25 22:20:01 +0600

    "LeetCode/Easy/228. Summary Ranges.cpp": update

--- LeetCode/Easy/228. Summary Ranges.cpp
+++ LeetCode/Easy/228. Summary Ranges.cpp
@@ -5,23 +5,36 @@ class Solution
 public:
     vector<string> summaryRanges(vector<int> &nums)
     {
-        /* [ 0, 1, 2, 4, 5, 7 ] */
+        /* [ 0, 2, 3, 4, 6, 8, 9 ] */
 
         int len = nums.size();
         vector<string> ans;
         string arrow = "->";
 
-        for (int i = 0; i < len; i++)
+        if (len < 1)
         {
-            string temp = to_string(nums[i]);
-            while (nums[i] + 1 == nums[i + 1])
+            return ans;
+        }
+        else if (len == 1)
+        {
+            ans.push_back(to_string(nums[0]));
+            return ans;
+        }
+        else
+        {
+            for (int i = 0; i < len; i++)
             {
-                i++;
+                string temp = to_string(nums[i]);
+                while (nums[i] + 1 == nums[i + 1])
+                {
+                    i++;
+                }
+                
+                temp += arrow + to_string(nums[i]);
+                ans.push_back(temp);
+                // cout << temp << endl;
+                temp = "";
             }
-            temp += arrow + to_string(nums[i]);
-            ans.push_back(temp);
-            //cout << temp << endl;
-            temp = "";
         }
         return ans;
     }
