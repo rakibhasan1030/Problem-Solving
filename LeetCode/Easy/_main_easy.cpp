17d6992f702c3c927e98bd543398c58e8329e8a6
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 00:56:06 +0600

    LeetCode/Easy/_main_easy: u

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -5,7 +5,7 @@ int main()
 {
     Solution s;
     vector<string> a;
-    vector<int> nums = {0,1,2,4,5,7};
+    vector<int> nums = {0, 1};
     a = s.summaryRanges(nums);
 
     for (size_t i = 0; i < a.size(); i++)
