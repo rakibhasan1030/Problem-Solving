f4e9b9295a9dc285f14f3043b2c63214888281f5
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-25 21:41:23 +0600

    "LeetCode/Easy/228. Summary Ranges.cpp": update

--- LeetCode/Easy/228. Summary Ranges.cpp
+++ LeetCode/Easy/228. Summary Ranges.cpp
@@ -5,12 +5,16 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         int len = nums.size();
         vector<string> ans;
+        string arrow = "->";
 
         for(int i  = 0; i < len; i++){
-            string temo = nums[i];
+            string temp = to_string(nums[i]);
             while(nums[i] + 1 == nums[i+1]){
                 i++;
             }
+            temp += arrow + to_string(nums[i]);
+            ans.push_back(temp);
+            temp = "";
         }
 
 
