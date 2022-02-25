a1eee6c08c79903880a67fb798045f8628d284d3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-25 21:49:23 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -4,8 +4,16 @@ using namespace std;
 int main()
 {
     Solution s;
+    vector<string> a;
     vector<int> nums = {0,1,2,4,5,7};
-    s.summaryRanges(nums);
+    a = s.summaryRanges(nums);
+
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        /* code */
+        cout << a[i] << ", ";
+    }
     
+
     return 0;
 }
