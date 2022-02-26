20865f602b08bace0096deb8aea67176bd504938
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 16:15:04 +0600

    LeetCode/Easy/_main_easy:

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
-#include "485. Max Consecutive Ones(Array).cpp"
+#include "717. 1-bit and 2-bit Characters(Arrray).cpp"
 using namespace std;
+
 int main()
 {
     Solution s;
     int a;
-    vector<int> nums = {0, 0};
-    a = s.findMaxConsecutiveOnes(nums);
+    vector<int> nums = {1, 0, 0};
+    a = s.isOneBitCharacter(nums);
     cout << "Ans = " << a << endl;
     return 0;
 }
