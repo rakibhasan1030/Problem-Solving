1330276f24536cc5286199f95903683dd7ad2385
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 23:26:30 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
-#include "896. Monotonic Array(Array).cpp"
+#include "674. Longest Continuous Increasing Subsequence(Array).cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    bool a;
-    vector<int> nums = {1, 2, 2, 3};
-    a = s.isMonotonic(nums);
+    int a;
+    vector<int> nums = {1, 3, 5, 4, 2, 3, 4, 5};
+    a = s.findLengthOfLCIS(nums);
     cout << "Ans = " << a << endl;
     return 0;
 }
