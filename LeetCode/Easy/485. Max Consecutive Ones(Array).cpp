f927de3f353e6a113d15ac0758e5bdcd9d9f24f8
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 12:23:34 +0600

    "LeetCode/Easy/485. Max Consecutive Ones(Array).cpp": update

--- LeetCode/Easy/485. Max Consecutive Ones(Array).cpp
+++ LeetCode/Easy/485. Max Consecutive Ones(Array).cpp
@@ -5,20 +5,29 @@ class Solution
 public:
     int findMaxConsecutiveOnes(vector<int> &nums)
     {
-        //nums = [ 1, 1, 0, 1, 1, 1 ]
+        // nums = [ 1, 1, 0, 1, 1, 1 ]
         int len = nums.size();
         int res;
-        if(len <= 1){
-            return len; 
-        }else{
-            for (int i = 0; i < len; i++){
-                res = 0;
-                while (i+1 <= len && nums[i] == nums[i+1])
+        if (len <= 1)
+        {
+            return len;
+        }
+        else
+        {
+            for (int i = 0; i < len; i++)
+            {
+                cout << "FOR (i) = " << i << endl;
+                res = 1;
+                while (i + 1 <= len && nums[i] == nums[i + 1])
                 {
                     /* code */
-                    res ++;
+                    res++;
+                    cout << "i = " << i << "   :   "
+                         << "res = " << res << endl;
                     i++;
                 }
+                cout << endl;
+                cout << endl;
             }
         }
         return res;
