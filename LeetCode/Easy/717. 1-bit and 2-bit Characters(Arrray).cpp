5c2c2a7dde0f089c894d3b91cbceb6ea4578e033
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 18:00:59 +0600

    "LeetCode/Easy/717. 1-bit and 2-bit Characters(Arrray).cpp": updater

--- LeetCode/Easy/717. 1-bit and 2-bit Characters(Arrray).cpp
+++ LeetCode/Easy/717. 1-bit and 2-bit Characters(Arrray).cpp
@@ -5,6 +5,14 @@ class Solution
 public:
     bool isOneBitCharacter(vector<int> &bits)
     {
-        
+        bool ans = false;
+        for (int i = 0; i < bits.size(); i++)
+        {
+            if (bits.size() - 1)
+                ans = true;
+            if (bits[i] == 1)
+                i++;
+        }
+        return ans;
     }
 };
