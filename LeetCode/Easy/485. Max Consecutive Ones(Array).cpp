428a87ada1c3e535308c137596ad126cf02e9bda
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 11:53:07 +0600

    "LeetCode/Easy/485. Max Consecutive Ones(Array).cpp": update

--- LeetCode/Easy/485. Max Consecutive Ones(Array).cpp
+++ LeetCode/Easy/485. Max Consecutive Ones(Array).cpp
@@ -5,5 +5,11 @@ class Solution
 public:
     int findMaxConsecutiveOnes(vector<int> &nums)
     {
+        int len = nums.size();
+        if(len <= 1){
+            retutn len; 
+        }else{
+            
+        }
     }
 };
