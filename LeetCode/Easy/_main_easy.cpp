f171c8a5d80a8e76c6ab7ffc171033f868ed0714
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 18:05:23 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -6,7 +6,7 @@ int main()
 {
     Solution s;
     int a;
-    vector<int> nums = {1, 0, 0};
+    vector<int> nums = {1, 1, 1, 0};
     a = s.isOneBitCharacter(nums);
     cout << "Ans = " << a << endl;
     return 0;
