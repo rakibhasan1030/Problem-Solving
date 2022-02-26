564d215c4b630805591ed917ed343ad8d8f9d434
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 23:26:19 +0600

    "LeetCode/Easy/674. Longest Continuous Increasing Subsequence(Array).cpp": solved

--- LeetCode/Easy/674. Longest Continuous Increasing Subsequence(Array).cpp
+++ LeetCode/Easy/674. Longest Continuous Increasing Subsequence(Array).cpp
@@ -6,9 +6,27 @@ public:
     int findLengthOfLCIS(vector<int> &nums)
     {
         int len = nums.size();
-        int n = 0;
+        int n = 1, mx = 1;
         for (int i = 0; i < len - 1; i++)
         {
+            if (nums[i] < nums[i + 1])
+            {
+                n++;
+                cout << n << "  ";
+                if (mx <= n)
+                {
+                    mx = n;
+                }
+            }
+            else
+            {
+                if (mx <= n)
+                {
+                    mx = n;
+                }
+                n = 1;
+            }
         }
+        return mx;
     }
 };
