513683063850e27c64d43c2ee8bb460fb01f5d3a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 23:03:26 +0600

    "LeetCode/Easy/896. Monotonic Array(Array).cpp": solved

--- LeetCode/Easy/896. Monotonic Array(Array).cpp
+++ LeetCode/Easy/896. Monotonic Array(Array).cpp
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool isMonotonic(vector<int>& nums) {
+    bool isMonotonic(vector<int> &nums)
+    {
         int len = nums.size();
-        bool ans = false;
-        for(auto num : nums){
-            
+        int increasing = 1, decreasing = 1;
+        for(int i = 0; i < len - 1; i++){
+            if (nums[i] <= nums[i+1]){
+                increasing++;
+            } 
+            if (nums[i] >= nums[i+1]){
+                decreasing++;
+            }
         }
+        if(increasing == len || decreasing == len){
+            return true;
+        }
+        return false;
     }
 };
