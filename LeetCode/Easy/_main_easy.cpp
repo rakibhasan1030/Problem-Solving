47024549980a532e7770a11ee0f4a4c6e0aa22f5
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 18:39:11 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
-#include "717. 1-bit and 2-bit Characters(Arrray).cpp"
+#include "896. Monotonic Array(Array).cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    int a;
-    vector<int> nums = {1, 1, 1, 0};
-    a = s.isOneBitCharacter(nums);
+    bool a;
+    vector<int> nums = {1, 2, 2, 3};
+    a = s.isMonotonic(nums);
     cout << "Ans = " << a << endl;
     return 0;
 }
