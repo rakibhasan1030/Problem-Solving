fa1f4af8ce50e9fbbb57f7c936708d9e5af37ca5
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-26 18:23:16 +0600

    "LeetCode/Easy/896. Monotonic Array(Array).cpp": update

--- LeetCode/Easy/896. Monotonic Array(Array).cpp
+++ LeetCode/Easy/896. Monotonic Array(Array).cpp
@@ -3,6 +3,10 @@ using namespace std;
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        
+        int len = nums.size();
+        bool ans = false;
+        for(auto num : nums){
+            
+        }
     }
 };
