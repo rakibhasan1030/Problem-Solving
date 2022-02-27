8b99b3f70749f1c846026f7845665342b03a5025
Rakib Hasan <rakibhasan1030@gmail.com>
2022-02-27 09:59:00 +0600

    "LeetCode/Easy/941. Valid Mountain Array(Array).cpp": solved

--- LeetCode/Easy/941. Valid Mountain Array(Array).cpp
+++ LeetCode/Easy/941. Valid Mountain Array(Array).cpp
@@ -1,10 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool validMountainArray(vector<int>& arr) {
-        int len = arr.size();
+    bool validMountainArray(vector<int> &arr)
+    {
+        int len, peek, peekPos;
+        len = arr.size();
+        bool res = false;
 
-        
+        peek = arr[0];
+        for (int i = 0; i < len; i++)
+        {
+            if (peek < arr[i])
+            {
+                peek = arr[i];
+                peekPos = i;
+            }
+        }
+        if (len >= 3 && arr[0] != peek && arr[len - 1] != peek)
+        {
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (arr[i] < arr[i + 1] && i <= peekPos || arr[i] > arr[i + 1] && i >= peekPos)
+                {
+                    res = true;
+                }
+                else
+                {
+                    res = false;
+                    break;
+                }
+            }
+        }
+        return res;
     }
 };
