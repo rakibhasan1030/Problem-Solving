e5699ee677b07297fffc838ec3e56e91b7671c34
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-02 00:46:07 +0600

    "LeetCode/Easy/1018. Binary Prefix Divisible By 5(Array).cpp": solved

--- LeetCode/Easy/1018. Binary Prefix Divisible By 5(Array).cpp
+++ LeetCode/Easy/1018. Binary Prefix Divisible By 5(Array).cpp
@@ -1,36 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    vector<bool> prefixesDivBy5(vector<int> &nums)
-    {
-        // [ 0 1 1 ]
-        vector<bool> ans;
-        int res = 0;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int exp = 0; 
-            for (int j = i; j >= 0; j--)
-            {
-                //cout << nums[j] << " " << "j = " << j<<endl;
-                res += nums[j] * pow(2, exp); 
-                //cout << "nums[" << j << "]" <<" * " << pow(2, exp) << "   = " << res << endl;
-                exp++;
-            }
-
-            cout << endl;
-            cout << endl;
-            //cout << res << endl;
-            if(res % 2 == 0){
-                ans.push_back(true);
-            }else{
-                ans.push_back(false);
-            }
-            res = 0;
-            exp = 0;
-        }
-        return ans;
-    }
+	vector<bool> prefixesDivBy5(vector<int>& nums) {
+		vector<bool> res;
+		int t = 0;
+		for(int i = 0; i < nums.size(); i++){
+			t = (t  * 2  + nums[i]) % 5;
+			res.push_back(t == 0);
+            cout << t << endl;
+		}
+		return res;
+	}
 };
