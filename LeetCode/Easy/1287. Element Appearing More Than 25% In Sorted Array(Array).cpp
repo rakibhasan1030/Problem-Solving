25977d596ed9ce6518d73d5ce22cb197ab7fafd6
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-02 01:52:36 +0600

    "LeetCode/Easy/1287. Element Appearing More Than 25% In Sorted Array(Array).cpp": solved

--- LeetCode/Easy/1287. Element Appearing More Than 25% In Sorted Array(Array).cpp
+++ LeetCode/Easy/1287. Element Appearing More Than 25% In Sorted Array(Array).cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int findSpecialInteger(vector<int>& arr) {
-        
+    int findSpecialInteger(vector<int> &arr)
+    {
+        //[ 1, 2, 2, 6, 6, 6, 6, 7, 10 ]
+        int n, p, largestDuplicate = 0, ans = arr[0];
+        n = n = arr.size();
+        p = n / 4;
+
+        for (int i = 0; i < n; i++)
+        {
+            int count = 1;
+            for (int j = i + 1; j <= n - 1; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (largestDuplicate <= count)
+            {
+                ans = arr[i];
+                largestDuplicate = count;
+            }
+            count = 1;
+        }
+        return largestDuplicate >= p ? ans : ans;
     }
 };
