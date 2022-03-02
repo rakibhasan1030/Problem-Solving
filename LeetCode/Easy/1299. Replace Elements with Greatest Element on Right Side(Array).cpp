0fe995d4558da6c59ad1b429203972e6f18d9527
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-03 00:04:59 +0600

    "LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp": solved

--- LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp
+++ LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp
@@ -6,34 +6,12 @@ public:
     vector<int> replaceElements(vector<int> &arr)
     {
         //[ 25, 25, 38, 20, 15, 40 ]
-        vector<int> ans;
-        int n = arr.size();
-        int temp = 0;
-
-        for(int i = 0; i < n; i++){
-            if(i == n - 1){
-                ans.push_back(-1);
-            }else{
-                //cout << "i = " << i << endl;
-                temp = arr[i+1];
-                int j = i;
-                int s = n-1;
-                for(int k = i+2; k < n; k++){
-                    //cout << "arr[k] = " << arr[k] << endl;
-                    if(arr[k] > temp){
-                        temp = arr[k];
-                        //cout << "temp = " << temp << endl;
-                    }
-                }
-                //cout << temp << endl;
-                ans.push_back(temp);
-                temp = 0;
-                j = 0;
-            }
+        int largest = -1;
+        for(int i = arr.size()-1; i >=0; --i){
+            int temp = arr[i];
+            arr[i] = largest;
+            largest = max(temp, largest);
         }
-        for(auto m : ans){
-            cout << m << "   ";
-        }
-        return ans;
+        return arr;
     }
 };
