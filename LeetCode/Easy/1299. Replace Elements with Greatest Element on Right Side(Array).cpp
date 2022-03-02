d0908591045961bfd392a5c60af7c016f97bb6cd
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-02 22:26:35 +0600

    "LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp": update

--- LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp
+++ LeetCode/Easy/1299. Replace Elements with Greatest Element on Right Side(Array).cpp
@@ -6,5 +6,10 @@ public:
     vector<int> replaceElements(vector<int> &arr)
     {
         //[ 17, 18, 5, 4, 6, 1 ]
+        int n = arr.size(), last = -1, temp = 0;
+        for(int i = 0; i < n; i++){
+            
+        }
+
     }
 };
