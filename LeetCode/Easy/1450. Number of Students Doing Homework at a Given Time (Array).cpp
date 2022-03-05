535d1c3ac7006f13fee95df7d008dde7390afa32
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 00:52:59 +0600

    "LeetCode/Easy/1450. Number of Students Doing Homework at a Given Time (Array).cpp": solved

--- LeetCode/Easy/1450. Number of Students Doing Homework at a Given Time (Array).cpp
+++ LeetCode/Easy/1450. Number of Students Doing Homework at a Given Time (Array).cpp
@@ -3,9 +3,9 @@ using namespace std;
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-        int res;
+        int res = 0;
         for (int i = 0; i < startTime.size(); i++){
-            if (startTime[i] <= queryTime && endTime[i] >= 4){
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime){
                 res++;
             }
         }
