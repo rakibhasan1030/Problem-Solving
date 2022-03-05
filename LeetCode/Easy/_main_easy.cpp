151410d9d65f8520be84addb7fb303110f4eacb3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 17:21:04 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     Solution s;
-    
+    vector<int> arr = {1, 2, 34, 3, 4, 5, 7, 23, 12};
+    cout << "Ans : " << s.distanceBetweenBusStops(arr) << endl;
     return 0;
 }
