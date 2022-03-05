c0f6841449fca3bef115078081cb5e0d4cdbe7a3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 17:57:26 +0600

    "LeetCode/Easy/1550. Three Consecutive Odds (Array).cpp": solved

--- LeetCode/Easy/1550. Three Consecutive Odds (Array).cpp
+++ LeetCode/Easy/1550. Three Consecutive Odds (Array).cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution{
 public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
-        
+    bool threeConsecutiveOdds(vector<int> &arr){
+        bool ans = false;
+        int c = 1;
+        for (int i = 1; i < arr.size(); i++){
+            if (c != 3){
+                if (arr[i - 1] % 2 == 1 && arr[i] % 2 == 1){
+                    ans = true;
+                    c++;
+                }
+                else{
+                    c = 1;
+                }
+            }else{
+                break;
+            }
+        }
+        return c >= 3 ? ans : false;
     }
 };
