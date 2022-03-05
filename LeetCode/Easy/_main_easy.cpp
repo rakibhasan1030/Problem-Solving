f9794d29fea0af9b1cc615b3edc85ad3082d9243
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 00:53:43 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
-#include "1450. Number of Students Doing Homework at a Given Time (Array).cpp"
+#include "1470. Shuffle the Array (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> startTime = {4};
-    vector<int> endTime = {4};
-    int queryTime = 5;
-    cout << "ANS : " << s.busyStudent(startTime, endTime, queryTime);
+    
     return 0;
 }
