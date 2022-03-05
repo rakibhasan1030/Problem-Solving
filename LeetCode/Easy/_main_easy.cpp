ce614148428ad3491a0d41966cbddb9d39f08f81
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 01:09:16 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -4,8 +4,13 @@ using namespace std;
 int main()
 {
     Solution s;
-    vector<int>& nums = {2,5,1,3,4,7};
+    vector<int> nums = {2,5,1,3,4,7};
     int n = 3;
-    
+
+    vector<int> ans = s.shuffle(nums, n);
+
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << "   "; 
+    }
     return 0;
 }
