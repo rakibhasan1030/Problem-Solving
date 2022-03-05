df7c596887717a8639c8779cd24acda264d6ed83
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 00:07:53 +0600

    LeetCode/Easy/_main_easy: solves

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {1, 0, 0, 0, 1, 0, 0, 1};
-    int k = 2;
+    vector<int> nums = {1,0,0,0};
+    int k = 1;
     cout << "Ans : " << s.kLengthApart(nums, k);
     return 0;
 }
