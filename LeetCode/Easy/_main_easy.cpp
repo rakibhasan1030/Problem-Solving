824c9d343b5dbc0dfdc45c8ba2675899d63b0268
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 00:32:22 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
-#include "1437. Check If All 1's Are at Least Length K Places Away (Array).cpp"
+#include "1450. Number of Students Doing Homework at a Given Time (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {1,0,0,0};
-    int k = 1;
-    cout << "Ans : " << s.kLengthApart(nums, k);
+    vector<int> startTime = {1,2,3};
+    vector<int> endTime = {3,2,7};
+    int queryTime = 4;
+    s.busyStudent(startTime, endTime, queryTime);
     return 0;
 }
