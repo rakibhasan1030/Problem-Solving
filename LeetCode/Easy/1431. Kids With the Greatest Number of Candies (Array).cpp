4c7dfbd672205fa8288f5879e4a76837ce79716c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-05 18:58:18 +0600

    "LeetCode/Easy/1431. Kids With the Greatest Number of Candies (Array).cpp": update

--- LeetCode/Easy/1431. Kids With the Greatest Number of Candies (Array).cpp
+++ LeetCode/Easy/1431. Kids With the Greatest Number of Candies (Array).cpp
@@ -3,9 +3,15 @@ using namespace std;
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        // candies = 2, 3, 5, 1, 3   |    extraCandies = 3
         vector<bool> ans;
         for (auto i : candies){
-            
+            int c = i + extraCandies;
+            for (auto i : candies){
+                if(i < extraCandies){
+                    
+                }
+            }
         }
     }
 };
