0c63f58daf1e53daaa7b3579ebc64a36fcb7b5ee
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 00:07:46 +0600

    "LeetCode/Easy/1437. Check If All 1's Are at Least Length K Places Away (Array).cpp": solved

--- LeetCode/Easy/1437. Check If All 1's Are at Least Length K Places Away (Array).cpp
+++ LeetCode/Easy/1437. Check If All 1's Are at Least Length K Places Away (Array).cpp
@@ -3,6 +3,21 @@ using namespace std;
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        
+        int pos = -1;
+        bool ans = true;
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[i] == 1 && pos >= 0){
+                if (k <= ((i+1) - pos) - 1){
+                    ans = true;
+                    pos = i+1;
+                }else{
+                    ans = false;
+                    break;
+                }
+            }else if (nums[i] == 1 && pos < 0){
+                pos = i+1;
+            }
+        }
+        return ans;
     }
 };
