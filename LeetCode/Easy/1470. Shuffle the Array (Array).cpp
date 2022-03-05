2734a546e2f2e5403b33a75777cf1db6ae6f473f
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 01:09:07 +0600

    "LeetCode/Easy/1470. Shuffle the Array (Array).cpp": solved

--- LeetCode/Easy/1470. Shuffle the Array (Array).cpp
+++ LeetCode/Easy/1470. Shuffle the Array (Array).cpp
@@ -3,6 +3,11 @@ using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        
+        vector<int> res;
+        for(int i = 0; i < n; i++){
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
+        }
+        return res;
     }
 };
