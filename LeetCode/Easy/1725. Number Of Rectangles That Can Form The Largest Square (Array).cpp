ff1be37fae151132906a6010bea4846261f5e95a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-06 12:23:56 +0600

    "LeetCode/Easy/1725. Number Of Rectangles That Can Form The Largest Square (Array).cpp": update

--- LeetCode/Easy/1725. Number Of Rectangles That Can Form The Largest Square (Array).cpp
+++ LeetCode/Easy/1725. Number Of Rectangles That Can Form The Largest Square (Array).cpp
@@ -3,6 +3,12 @@ using namespace std;
 class Solution {
 public:
     int countGoodRectangles(vector<vector<int>>& rectangles) {
-        
+        vector<int> unique; 
+        int m; 
+        for(auto rec : rectangles){
+            int j = 0;
+            cout << max(rec[j], rec[j+1]);
+            cout<<endl;
+        }
     }
 };
