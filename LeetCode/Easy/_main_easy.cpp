c0303e944a075763a48d5874753dab7fd0f1326c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-07 15:40:32 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
-#include "1752. Check if Array Is Sorted and Rotated (Array).cpp"
+#include "1184. Distance Between Bus Stops (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> n = {3, 4, 5, 1, 2};
-    bool ans = s.check(n);
-    ans == true ? cout << "TRUE" : cout << "FALSE"; 
+
     return 0;
 }
