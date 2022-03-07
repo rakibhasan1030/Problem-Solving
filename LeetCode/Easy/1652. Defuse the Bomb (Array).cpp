7589cd50f3bdf50d67d51fc5c2ef13d01251ff39
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-08 00:17:48 +0600

    "LeetCode/Easy/1652. Defuse the Bomb (Array).cpp": update

--- LeetCode/Easy/1652. Defuse the Bomb (Array).cpp
+++ LeetCode/Easy/1652. Defuse the Bomb (Array).cpp
@@ -3,6 +3,42 @@ using namespace std;
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        
+        // 2, 4, 9, 3
+        int len = code.size();
+        vector<int> res;
+
+        for(int i = 0; i < len; i++){
+            int key = abs(k), pos = i, temp = 0;
+
+            if(k == 0){
+                int l = len;
+                while (l){
+                    res.push_back(k);
+                }
+            }
+
+            if(k > 0){
+                while(key){
+                    temp += code[(pos+1)%code.size()];
+                    key--;
+                    pos++;
+                }
+                res.push_back(temp);
+            } 
+
+            if(k < 0){
+                while(key){
+                    temp += code[(code.size()-(pos+1))%code.size()];
+                    key--;
+                    pos++;
+                }
+                res.push_back(temp);
+            } 
+        }
+
+        for(auto i : res){
+            cout << i << "     ";
+        }
+
     }
 };
