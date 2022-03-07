ecf955879b25cd9eeca3fdf148547656b1ce057b
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-07 15:28:37 +0600

    "LeetCode/Easy/1752. Check if Array Is Sorted and Rotated (Array).cpp": uypdate

--- LeetCode/Easy/1752. Check if Array Is Sorted and Rotated (Array).cpp
+++ LeetCode/Easy/1752. Check if Array Is Sorted and Rotated (Array).cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution{
 public:
-    bool check(vector<int>& nums) {
-        
+    bool check(vector<int> &nums){
+        // 3, 4, 5, 1, 2
+        int c = 0; 
+
+        return c > 1 ? false : true;
     }
 };
