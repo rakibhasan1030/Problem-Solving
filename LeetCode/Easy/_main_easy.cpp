784902c2b9cb3de25fca8e3e8c3e97637fe5ad20
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-07 23:08:19 +0600

    LeetCode/Easy/_main_easy: update

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
-#include "1184. Distance Between Bus Stops (Array).cpp"
+#include "1652. Defuse the Bomb (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> distance = {1, 2, 3, 4};
-    int start = 0, destination = 1;
 
     return 0;
 }
