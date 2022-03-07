3ce36e5d2449c0cccbd0a4b133f5eeee5fb8fd10
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-07 17:55:32 +0600

    "LeetCode/Easy/1184. Distance Between Bus Stops (Array).cpp": solved

--- LeetCode/Easy/1184. Distance Between Bus Stops (Array).cpp
+++ LeetCode/Easy/1184. Distance Between Bus Stops (Array).cpp
@@ -3,6 +3,17 @@ using namespace std;
 class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-        
+        int clockwiseDistance = 0, totalDistance = 0;
+
+        for(int i = 0; i < distance.size(); i++){
+            if (start < destination && (i >= start && i < destination)){
+                clockwiseDistance  += distance[i];
+            }
+            if (start > destination && (i >= start || i < destination)){
+                clockwiseDistance  += distance[i];
+            }
+            totalDistance += distance[i];
+        }
+        return min(clockwiseDistance, totalDistance - clockwiseDistance);
     }
 };
