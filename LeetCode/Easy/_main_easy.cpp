35c9a7b60462523a3772a9c73ab9f762269f1d05
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-07 15:30:24 +0600

    LeetCode/Easy/_main_easy: solved

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -5,6 +5,7 @@ int main()
 {
     Solution s;
     vector<int> n = {3, 4, 5, 1, 2};
-    bool ans = s.check(n)
+    bool ans = s.check(n);
+    ans == true ? cout << "TRUE" : cout << "FALSE"; 
     return 0;
 }
