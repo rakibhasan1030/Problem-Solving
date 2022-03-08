79c3531114ed22c58aedb4c556065579312f969f
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 01:39:07 +0600

    "LeetCode/Easy/1909. Remove One Element to Make the Array Strictly Increasing (Array).cpp":

--- LeetCode/Easy/1909. Remove One Element to Make the Array Strictly Increasing (Array).cpp
+++ LeetCode/Easy/1909. Remove One Element to Make the Array Strictly Increasing (Array).cpp
@@ -3,6 +3,22 @@ using namespace std;
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        
+        //1,2,10,5,7
+        vector<int> n;
+        n.push_back(n[0]);
+        for(int i = 1; i < nums.size(); i++){
+            cout <<  " if  nums[i] = " << nums[i] << endl;
+            if (nums[i] > nums[i -1]){
+                 cout <<  " if  nums[i] = " << nums[i] << endl;
+                n.push_back(nums[i]);
+            }else{
+                cout <<  " else  nums[i] = " << nums[i] << endl;
+                continue;
+            }
+        }
+
+        for (auto i : n){
+            cout << i << "   ";
+        }
     }
 };
