eb2e911e1c5721457e6329c488c8edca16b73224
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-08 23:56:15 +0600

    "LeetCode/Easy/1779. Find Nearest Point That Has the Same X or Y Coordinate (Array).cpp": solved

--- LeetCode/Easy/1779. Find Nearest Point That Has the Same X or Y Coordinate (Array).cpp
+++ LeetCode/Easy/1779. Find Nearest Point That Has the Same X or Y Coordinate (Array).cpp
@@ -3,24 +3,21 @@ using namespace std;
 class Solution {
 public:
     int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
-        vector<vector<int>> validCoordinates;
-        int dis = -1;
+        int dis = INT_MAX;
+        int ans = -1;
         for(int i = 0; i < points.size(); i++){
-
             for (int j = 0; j < points[i].size() - 1; j++){
                 if(x == points[i][j] || y == points[i][j+1]){
                     int d = 0;
-                    cout <<"[ " << points[i][j] << "  " << points[i][j+1] << " ]" << endl;
                     d = (abs(x - points[i][j]) + abs(y - points[i][j+1]));
-                    cout << "Min = " << d;
-                    if(dis < d){
+                    if(d < dis){
                         dis = d;
+                        ans = i;
                     }
                 }
-                cout << endl;
-                cout << endl;
-                cout << "Last Min = " << dis;
             }
+            cout << "ans = " << ans << endl;
         }
+        return ans;
     }
 };
