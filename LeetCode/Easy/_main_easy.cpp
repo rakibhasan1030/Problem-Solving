5c04829d3e1bebff844be22fb1be82013ad81fe2
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 01:05:58 +0600

    LeetCode/Easy/_main_easy:

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-#include "1800. Maximum Ascending Subarray Sum (Array).cpp"
+#include "1848. Minimum Distance to the Target Element (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {10, 20, 30, 5, 10, 50};
-    cout<< s.maxAscendingSum(nums) << endl;
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int target = 5, start = 3;
+    cout << s.getMinDistance(nums, target, start) << endl;
     return 0;
 }
