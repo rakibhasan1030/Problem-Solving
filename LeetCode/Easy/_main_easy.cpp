cb7ef29eb0fa5a35ef6967f8f225aeaa35cb8ece
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 01:24:58 +0600

    LeetCode/Easy/_main_easy:

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {1, 2, 3, 4, 5};
-    int target = 5, start = 3;
+    vector<int> nums = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int target = 1, start = 0;
     cout << s.getMinDistance(nums, target, start) << endl;
     return 0;
 }
