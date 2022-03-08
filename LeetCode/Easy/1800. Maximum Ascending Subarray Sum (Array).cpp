41f328e325cac092e060bb66fad470116e1c455c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 00:56:19 +0600

    "LeetCode/Easy/1800. Maximum Ascending Subarray Sum (Array).cpp": solved

--- LeetCode/Easy/1800. Maximum Ascending Subarray Sum (Array).cpp
+++ LeetCode/Easy/1800. Maximum Ascending Subarray Sum (Array).cpp
@@ -3,6 +3,11 @@ using namespace std;
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        
+        int temp = nums[0], mx = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            temp = nums[i] > nums[i-1] ? temp += nums[i] : nums[i];
+            mx = max(mx, temp);
+        }
+        return mx;
     }
 };
