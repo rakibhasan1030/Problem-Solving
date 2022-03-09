9431011b60c2d0d63d4c0abfa35105bb2099e6f9
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-10 01:52:38 +0600

    "LeetCode/Easy/521. Longest Uncommon Subsequence I (String).cpp":

--- LeetCode/Easy/521. Longest Uncommon Subsequence I (String).cpp
+++ LeetCode/Easy/521. Longest Uncommon Subsequence I (String).cpp
@@ -3,10 +3,26 @@ using namespace std;
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        for(int i = 0; i < a.length(); i++){
-            for(int i = 0; i < a.length(); i++){
-            
-        }   
+        if(a == b) return -1;
+        int n = a.size();
+        int m = b.size();
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<=n;i++) {
+            for(int j=0;j<=m;j++) {
+                if(i == 0 && j == 0) 
+                    dp[i][j] = 0;
+                else if(i==0) 
+                        dp[i][j] = j;
+                else if(j==0) 
+                       dp[i][j] = i; 
+                else if(a[i-1] != b[j-1]) 
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
         }
+        return dp[n][m];
     }
 };
