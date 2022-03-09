7eee2c6896ecf1342c3463f90edc575f0644e9d0
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 15:03:03 +0600

    "LeetCode/Easy/1929. Concatenation of Array (Array).cpp": solved

--- LeetCode/Easy/1929. Concatenation of Array (Array).cpp
+++ LeetCode/Easy/1929. Concatenation of Array (Array).cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution{
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        
+    vector<int> getConcatenation(vector<int>& nums){
+        vector<int> ans;
+        int len = 2;
+        while (len){
+            for(auto n : nums){
+                ans.push_back(n);
+            }
+            len--;
+        }
+        return ans;
     }
 };
