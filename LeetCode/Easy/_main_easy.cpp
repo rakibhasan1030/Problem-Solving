c85ea872ebd57ba862407c13ad5becc3826ea947
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-10 00:37:39 +0600

    LeetCode/Easy/_main_easy:

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
-#include "2057. Smallest Index With Equal Value (Array).cpp"
+#include "521. Longest Uncommon Subsequence I (String).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {0, 1, 2};
-    cout << s.smallestEqual(nums);
+    
     return 0;
 }
