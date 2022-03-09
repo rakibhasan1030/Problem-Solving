90aa5655a1b7e2e2208cba3dfb52da6cd8d348ae
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 14:55:04 +0600

    LeetCode/Easy/_main_easy:

--- LeetCode/Easy/_main_easy.cpp
+++ LeetCode/Easy/_main_easy.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
-#include "1909. Remove One Element to Make the Array Strictly Increasing (Array).cpp"
+#include "1929. Concatenation of Array (Array).cpp"
 using namespace std;
 int main()
 {
     Solution s;
-    vector<int> nums = {1, 2, 10, 5, 7};
-    int target = 1, start = 0;
-    cout << s.canBeIncreasing(nums) << endl;
+    
     return 0;
 }
