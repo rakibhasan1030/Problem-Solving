20bdebf3c649517ae0dd102d566661202ed1a74a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-09 18:12:23 +0600

    "LeetCode/Easy/2016. Maximum Difference Between Increasing Elements (Array).cpp":

--- LeetCode/Easy/2016. Maximum Difference Between Increasing Elements (Array).cpp
+++ LeetCode/Easy/2016. Maximum Difference Between Increasing Elements (Array).cpp
@@ -3,6 +3,13 @@ using namespace std;
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        
+        //7, 1, 5, 4
+        int mx = 0;
+        for(int i = 1; i < nums.size()-1; i++){
+            
+            mx = max(mx, (nums[i] - nums[i-1]));
+
+        }
+        return mx;
     }
 };
