4180dddae04cd99f32dfa0c6f5ca4d8891f3c507
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 00:27:18 +0600

    "LeetCode/Easy/1446. Consecutive Characters (String).cpp": solved

--- LeetCode/Easy/1446. Consecutive Characters (String).cpp
+++ LeetCode/Easy/1446. Consecutive Characters (String).cpp
@@ -1,11 +1,18 @@
-int _temp = 1, _max = 1;
-if(s.size() == 1) return 1;
-for(int i = 1; i <= s.size(); i++){
-	if (s[i-1] == s[i]){
-		_temp++;
-	}else{
-		_temp = 1;
-	}
-	if(_temp > _max) _max = _temp;
-}
-return _max;
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxPower(string s) {
+    int _temp = 1, _max = 1;
+    if(s.size() == 1) return 1;
+    for(int i = 1; i <= s.size(); i++){
+        if (s[i-1] == s[i]){
+            _temp++;
+        }else{
+            _temp = 1;
+        }
+        if(_temp > _max) _max = _temp;
+    }
+    return _max;
+    }
+};
