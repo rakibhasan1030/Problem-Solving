1c68ffb9aa29c3415e58021b04193c19b44adf75
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 00:20:48 +0600

    "LeetCode/Easy/1221. Split a String in Balanced Strings (String).cpp": solved

--- LeetCode/Easy/1221. Split a String in Balanced Strings (String).cpp
+++ LeetCode/Easy/1221. Split a String in Balanced Strings (String).cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int balancedStringSplit(string s) {
     int s_size = s.size();
     int r_count = 0, l_count = 0, main_count = 0;
     for(int i = 0; i < s_size; i++)
@@ -15,3 +20,5 @@
     }
     cout<< main_count << endl;
     return main_count;
+    }
+};
