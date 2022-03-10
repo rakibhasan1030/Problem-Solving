b35674ebe97084bb35db1fbeed37dc6581b3e7ef
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 00:41:37 +0600

    "LeetCode/Easy/2124. Check if All A's Appears Before All B's (String).cpp": solved

--- LeetCode/Easy/2124. Check if All A's Appears Before All B's (String).cpp
+++ LeetCode/Easy/2124. Check if All A's Appears Before All B's (String).cpp
@@ -1,6 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkString(string s) {
     for (int i = 1; i < s.size(); i++){
         if (s[i] < s[i-1]){
             return false;
         }
     }
     return true;
+    }
+};
