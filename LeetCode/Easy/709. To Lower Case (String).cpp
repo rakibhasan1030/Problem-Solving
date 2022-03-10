d8056e55bf3c0a87b91490c307b7d6ee5fb25960
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 00:14:25 +0600

    "LeetCode/Easy/709. To Lower Case (String).cpp": solved

--- LeetCode/Easy/709. To Lower Case (String).cpp
+++ LeetCode/Easy/709. To Lower Case (String).cpp
@@ -1,2 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string toLowerCase(string s) {
     transform(s.begin(), s.end(), s.begin(), ::tolower);
     return s;
+    }
+};
