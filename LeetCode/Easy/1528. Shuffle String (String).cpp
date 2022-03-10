43d9d65e19b186e3c3e76781fc08c36b3dbe82cf
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 00:28:19 +0600

    "LeetCode/Easy/1528. Shuffle String (String).cpp": solved

--- LeetCode/Easy/1528. Shuffle String (String).cpp
+++ LeetCode/Easy/1528. Shuffle String (String).cpp
@@ -1,7 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+    
     string str = "";
     int arrSize = indices.size();
     for(int i = 0; i < arrSize; i++)
     {
+
         for(int j = 0; j < arrSize; j++)
         {
             if(i == indices[j])
@@ -9,5 +16,9 @@
                 str = str + s[j];
             }
         }
+
+
     }
     return str;
+    }
+};
