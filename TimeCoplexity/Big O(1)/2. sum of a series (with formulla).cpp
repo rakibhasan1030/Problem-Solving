b0e713d4fbb79ae582e22e680952569e189ba90d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 19:32:05 +0600

    "TimeCoplexity/Big O(1)/2. sum of a series (with formulla).cpp": update

--- TimeCoplexity/Big O(1)/2. sum of a series (with formulla).cpp
+++ TimeCoplexity/Big O(1)/2. sum of a series (with formulla).cpp
@@ -1,19 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n, result;
 
-    cin>> n;
+    cin >> n;
 
-    result = n * (n+12) / 2; // here, three mathmatical operation occur( * , + , /) and one assignment operation(=)
+    result = n * (n + 12) / 2; // here, three mathmatical operation occur( * , + , /) and one assignment operation(=)
 
-    cout << result; //this is not a complexity, because, it'll execute once
+    cout << result; // this is not a complexity, because, it'll execute once
 
     return 0;
 }
 
 /*
-    In this program total operation is 4 and It'll be always 4. Beacause, here number of operation is not dependent on input.
-    Time complexity of this program is O(1).
+    In this program total operation number is 4 and It'll be always 4. Beacause, here number of operation is not dependent on input.
+    Input is not constanst, but for any input operation number will be 4. So, the time complexity of this program is O(1).
     Time complexity of a program will always Big O(1) or O(1) or Order(1), when the number of operation is not dependent on input of a program.
 */
