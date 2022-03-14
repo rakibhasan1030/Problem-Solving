da621c447feba035186b2fb02e6f6ca89f0bb75d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-14 17:48:14 +0600

    "LeetCode/Easy/83. Remove Duplicates from Sorted List.cpp": Solved

--- LeetCode/Easy/83. Remove Duplicates from Sorted List.cpp
+++ LeetCode/Easy/83. Remove Duplicates from Sorted List.cpp
@@ -9,8 +9,20 @@ using namespace std;
 
 class Solution {
 public:
-    void deleteNode(ListNode* node) {
-        node->val = (node->next)->val;
-        node->next = (node->next)->next;
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head) return head;
+        
+        ListNode *temp = head, *mLeak;
+        
+        while(temp && temp->next){
+             if(temp->val == (temp->next)->val){
+                 mLeak = temp->next;
+                 temp->next = mLeak->next; 
+                 delete mLeak;
+             }else{
+                 temp = temp->next;
+             }
+        }
+        return head;
     }
 };
