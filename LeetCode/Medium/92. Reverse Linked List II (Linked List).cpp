5f823800f2ec3a5a24112a399fb90b0f068c2cfa
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-16 16:46:37 +0600

    "LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp":

--- LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp
+++ LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp
@@ -1,2 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        
+    }
+};
