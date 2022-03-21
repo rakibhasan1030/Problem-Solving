ea24f3a51d034517284168518c7376e4d8e2465d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-22 01:40:57 +0600

    "LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp": solved

--- LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp
+++ LeetCode/Medium/92. Reverse Linked List II (Linked List).cpp
@@ -1,19 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-class Solution
-{
+
+class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int left, int right)
-    {
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        if(!head || left == right) return head;
         
+        ListNode dummyHead(0);
+        dummyHead.next = head;
+        
+        auto* nodeBeforeSublist = &dummyHead;
+        int pos = 1;
+        while(pos < left){
+            nodeBeforeSublist = nodeBeforeSublist->next;
+            pos++;
+        }
+        auto* workingPtr = nodeBeforeSublist->next;
+        while(left < right) {
+
+            // cut the new node out
+            auto* nodeToBeExtracted = workingPtr->next;
+            workingPtr->next = nodeToBeExtracted->next;  
+            
+            // pasting it at the front
+            nodeToBeExtracted->next = nodeBeforeSublist->next;
+            nodeBeforeSublist->next = nodeToBeExtracted;
+            
+            left++;
+        }
+        
+        return dummyHead.next;
     }
 };
