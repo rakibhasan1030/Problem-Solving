1689e2576567176fdb6d258a9b531754c37a1203
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-24 01:48:25 +0600

    LeetCode/Medium/_main_medium:

--- LeetCode/Medium/_main_medium.cpp
+++ LeetCode/Medium/_main_medium.cpp
@@ -1,9 +1,67 @@
 #include <bits/stdc++.h>
-#include "1143. Longest Common Subsequence.cpp"
 using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *&head)
+{
+    if (head == NULL)
+        return;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
 int main()
 {
-    Solution s;
-    cout << s.longestCommonSubsequence("abcde", "ace");
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    cout << endl;
+    cout << endl;
+
+    display(head);
+    cout << endl;
+    cout << endl;
+
+    /* call desire problems method */
+
+    display(head);
+    cout << endl;
+    cout << endl;
     return 0;
 }
