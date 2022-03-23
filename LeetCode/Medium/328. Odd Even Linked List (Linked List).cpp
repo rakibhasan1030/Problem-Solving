e31446d76ef9cecc2e6bdd1d4fd6e4869dcf6b89
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-24 01:52:41 +0600

    "LeetCode/Medium/328. Odd Even Linked List (Linked List).cpp": create

---
+++ LeetCode/Medium/328. Odd Even Linked List (Linked List).cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    node* oddEvenList(node* head) {
+        
+    if (head == NULL || head->next == NULL || head->next->next == NULL) return head; /* Boundary Cases*/
+
+    auto *odd = head;
+    auto *even = head->next;
+    auto *evenStart = head->next;
+
+    while (even->next != NULL && odd->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        if (odd->next != NULL){
+            even = even->next;
+        }
+            
+    }
+    odd->next = evenStart;
+    return head; 
+    }
+};
