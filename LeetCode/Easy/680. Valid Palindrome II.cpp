df43be8ab59140634c11f787205fa431bcc97a18
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-02 13:16:38 +0600

    "LeetCode/Easy/680. Valid Palindrome II.cpp": Solved

--- LeetCode/Easy/680. Valid Palindrome II.cpp
+++ LeetCode/Easy/680. Valid Palindrome II.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 class Solution {
 public:
-    bool validPalindrome(string s) {
-        
+
+    bool isPalindrome(string s, int i, int j){
+        while(i < j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }else return false;
+        }
+        return true;
     }
+
+    bool validPalindrome(string s) {
+        if (s.size() < 3 ) return true; /* Boundary cases */
+
+        int i = 0, j = s.size() -1;
+
+        while(i < j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }else{
+                /* here, we delete the index by increasing i or decerasing j and checck for palindrome */
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+        }
+        return true;
+    }
+    
 };
