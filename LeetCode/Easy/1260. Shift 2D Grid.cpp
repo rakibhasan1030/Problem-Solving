bd4ed56ce65d74f60b7252da0af49d5c847fc51e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-16 23:58:07 +0600

    "LeetCode/Easy/1260. Shift 2D Grid.cpp":

--- LeetCode/Easy/1260. Shift 2D Grid.cpp
+++ LeetCode/Easy/1260. Shift 2D Grid.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        
+    vector<vector<int>> shiftGrid(vector<vector<int>>& m, int k) {
+        int r = m.size(), c = m[0].size();
+        vector<vector<int>> ans(r, vector<int>(c, 0));
+        for(int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                
+                int newPos = ( ( (i*c)+j) + k ) % (r *c ); /*   >> (i*c)+j is for find index in 1D array
+                                                                >> adding k is shifting the value to the k position, 
+                                                                   so after adding k, it'll be new position in 1D array 
+                                                                >> (r * c) is for outOfBoundExeption of the array */
+                
+                /* convert 1D array newPos to 2D matrix positions, newPos = [newRow, newCol] */
+                int newRow = newPos / c;
+                int newCol = newPos % c;
+                ans[newRow][newCol] = m[i][j];
+            }
+        }
+        return ans;
     }
 };
+/*DETAIL EXPLANATION : https://youtu.be/esu6uExE0qg */
+
