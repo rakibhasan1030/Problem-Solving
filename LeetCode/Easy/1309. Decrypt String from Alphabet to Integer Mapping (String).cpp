5ea9d87325323f12a66a52b9190fb7167cc9dc71
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-30 11:53:42 +0600

    "LeetCode/Easy/1309. Decrypt String from Alphabet to Integer Mapping (String).cpp":

--- LeetCode/Easy/1309. Decrypt String from Alphabet to Integer Mapping (String).cpp
+++ LeetCode/Easy/1309. Decrypt String from Alphabet to Integer Mapping (String).cpp
@@ -6,16 +6,13 @@ public:
     string ans = "";
     int len = s.size();
 
-    for(int i = 0; i < len; ++i)
-    {
+    for(int i = 0; i < len; ++i){
         string temp_str = "";
         int temp_int = 0;
         char temp_char;
 
-        if(len >= (i + 2))
-        {
-            if (s[i+2] == '#')
-            {
+        if(len >= (i + 2)){
+            if (s[i+2] == '#'){
                 temp_str.push_back(s[i]);
                 temp_str.push_back(s[i+1]);
                 temp_int = stoi(temp_str) + 96;
@@ -23,17 +20,13 @@ public:
                 ans.push_back(temp_char);
                 cout<<temp_char<<endl;
                 i += 2;
-            }
-            else
-            {
+            }else{
                 temp_str.push_back(s[i]);
                 temp_int = stoi(temp_str) + 96;
                 temp_char  = (char)temp_int;
                 ans.push_back(temp_char);
             }
-        }
-        else
-        {
+        }else{
             temp_str.push_back(s[i]);
             temp_int = stoi(temp_str) + 96;
             temp_char  = (char)temp_int;
@@ -42,5 +35,5 @@ public:
 
     }
     return ans;
-    }
+}
 };
