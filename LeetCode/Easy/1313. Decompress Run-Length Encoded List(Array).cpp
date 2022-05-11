20de72bee74888bc5dd69b3a0ec3993c67b22474
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-11 20:28:29 +0600

    "LeetCode/Easy/1313. Decompress Run-Length Encoded List(Array).cpp":

--- LeetCode/Easy/1313. Decompress Run-Length Encoded List(Array).cpp
+++ LeetCode/Easy/1313. Decompress Run-Length Encoded List(Array).cpp
@@ -3,14 +3,13 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> decompressRLElist(vector<int> &nums)
-    {
+    vector<int> decompressRLElist(vector<int> &nums) {
         vector<int> ans;
         int len = nums.size();
-        for (int i = 0; i < len; i+=2){
+        for (int i = 0; i < len; i+=2) {
             int freq = nums[i];
             int val = nums[i + 1];
-            for (int j = 0; j < freq; j++){
+            for (int j = 0; j < freq; j++) {
                 ans.push_back(val);
             }
         }
