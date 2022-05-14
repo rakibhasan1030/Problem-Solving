d56f2028e5bf066e75511c28aaf5bf377542e567
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-15 01:43:30 +0600

    "LeetCode/Easy/344. Reverse String.cpp":

--- LeetCode/Easy/344. Reverse String.cpp
+++ LeetCode/Easy/344. Reverse String.cpp
@@ -5,7 +5,7 @@ public:
     void reverseString(vector<char>& s) {
         int size = s.size();
         char temp;
-        for (int i = 0; i < size/2; i++){
+        for (int i = 0; i < size/2; i++) {
             temp = s[i];
             s[i] = s[size-i-1];
             s[size-i-1] = temp;
