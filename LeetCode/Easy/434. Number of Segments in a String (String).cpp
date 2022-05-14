cc8ef6d57cb278dac0d60e9a2bbc3f609d58fe49
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-15 01:43:34 +0600

    "LeetCode/Easy/434. Number of Segments in a String (String).cpp":

--- LeetCode/Easy/434. Number of Segments in a String (String).cpp
+++ LeetCode/Easy/434. Number of Segments in a String (String).cpp
@@ -11,7 +11,7 @@ public:
     int countSegments(string s) {
     int word = 0;
     for(int i = 0; i < s.size(); i ++){
-        if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0'){
+        if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0') {
             word++;
         }
     } 
