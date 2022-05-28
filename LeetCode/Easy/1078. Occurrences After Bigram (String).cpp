b89bae1949e93cdeb0bc9b04b18a2e75e6107dee
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-29 00:46:50 +0600

    "LeetCode/Easy/1078. Occurrences After Bigram (String).cpp":

--- LeetCode/Easy/1078. Occurrences After Bigram (String).cpp
+++ LeetCode/Easy/1078. Occurrences After Bigram (String).cpp
@@ -11,7 +11,7 @@ public:
         } 
         int n = store.size();
         for(int i=0;i<n;i++){
-            if(store[i]==first && store[i+1]==second && i+2<n){
+            if(store[i]==first && store[i+1]==second && i+2<n) {
                 ans.push_back(store[i+2]);
             }
         }
