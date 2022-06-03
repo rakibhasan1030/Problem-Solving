a7bd20f13fcbf2cb67487654c49cd3f9e98bd413
Rakib Hasan <rakibhasan1030@gmail.com>
2022-06-04 00:05:54 +0600

    "LeetCode/Easy/1295. Find Numbers with Even Number of Digits (Array).cpp":

--- LeetCode/Easy/1295. Find Numbers with Even Number of Digits (Array).cpp
+++ LeetCode/Easy/1295. Find Numbers with Even Number of Digits (Array).cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int c = 0;
-        for (auto i : nums){
+        for (auto i : nums) {
             if(to_string(i).size() % 2 == 0) c += 1;  
         }
         return c;
