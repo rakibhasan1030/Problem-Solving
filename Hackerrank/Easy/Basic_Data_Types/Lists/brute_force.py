1611bb91ee0bbbfa1fd5df6546ed4b67103e38e6
Rakib Hasan <rakibhasan1030@gmail.com>
2022-12-06 01:24:50 +0600

    Hackerrank/Easy/Basic_Data_Types/Lists/brute_force:

--- Hackerrank/Easy/Basic_Data_Types/Lists/brute_force.py
+++ Hackerrank/Easy/Basic_Data_Types/Lists/brute_force.py
@@ -1,1 +1,10 @@
 # Problem : Lists (https://www.hackerrank.com/challenges/python-lists/problem)
+
+if __name__ == '__main__': 
+    def print_list(l):
+        print(l)
+
+    N = int(input())
+    
+
+
